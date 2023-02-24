6ac3cabc6f8f296e01ecf4ef54714b31460b3e35
Artsem-Lisitsa <lisitsaartsem1526al@gmail.com>
2023-02-24 13:54:44 +0300

    start form has been redesigned

--- src/main/java/com/sulitsa/dev/accountant/controller/StartController.java
+++ src/main/java/com/sulitsa/dev/accountant/controller/StartController.java
@@ -1,11 +1,15 @@
 package com.sulitsa.dev.accountant.controller;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 
 import com.sulitsa.dev.accountant.view.QuestionApplication;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
 public class StartController {
@@ -15,9 +19,15 @@ public class StartController {
 
     @FXML
     private Button startBtn;
+    @FXML
+    private ImageView exitButtonImg, ruleButtonImg, startButtonImg;
 
     @FXML
-    void initialize() {
+    void initialize() throws FileNotFoundException {
+        exitButtonImg.setImage(new Image(new FileInputStream("src/main/resources/images/menu_button.png")));
+        ruleButtonImg.setImage(new Image(new FileInputStream("src/main/resources/images/menu_button.png")));
+        startButtonImg.setImage(new Image(new FileInputStream("src/main/resources/images/menu_button.png")));
+
         exitBtn.setOnAction(event -> {
             Platform.exit();
             System.exit(0);
